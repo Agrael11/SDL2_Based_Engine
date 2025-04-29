07d6a3231f2699056e0df9e9175eb52216b03f19
Tachi <Oliver.Neuschl@gmail.com>
2025-04-29 16:43:53 +0200

    Added MaterialManager

---
+++ src/Engine/Managers/MaterialManager.cpp
@@ -0,0 +1,70 @@
+#include "MaterialManager.h"
+
+#include <unordered_map>
+#include <memory>
+#include "../Helper/Logger.h"
+
+#include "../Rendering/Material.h"
+
+
+using namespace Engine::Helper;
+using namespace Engine::Managers;
+using namespace Engine::Rendering;
+
+
+std::unordered_map<std::string, std::unique_ptr<Material>> m_materials;
+
+MaterialManager::MaterialManager()
+{
+}
+
+bool MaterialManager::AddMaterial(std::string id, Material& shader)
+{
+	if (MaterialExists(id))
+	{
+		Logger::log(Logger::Level::Error, "Material {} already loaded", id);
+		return false;
+	}
+
+	m_materials[id] = std::make_unique<Material>(std::move(shader));
+	return true;
+}
+
+bool MaterialManager::MaterialExists(std::string id)
+{
+	return (m_materials.count(id) > 0);
+}
+
+Material& MaterialManager::GetMaterial(std::string id)
+{
+	if (!MaterialExists(id))
+	{
+		throw std::runtime_error("Material " + id + " does not exist");
+	}
+	return *m_materials[id];
+}
+
+bool MaterialManager::RemoveShader(std::string id)
+{
+	if (!MaterialExists(id))
+	{
+		Logger::log(Logger::Level::Error, "Material {} does not exist", id);
+		return false;
+	}
+
+	m_materials[id].reset();
+	m_materials.erase(id);
+	Logger::log(Logger::Level::Info, "Removed {} from Material Manager", id);
+	return true;
+}
+
+bool MaterialManager::RemoveAll()
+{
+	for (auto& shader : m_materials)
+	{
+		shader.second.reset();
+	}
+	m_materials.clear();
+	Logger::log(Logger::Level::Info, "Removed all materials from Material Manager");
+	return true;
+}
---
+++ src/Engine/Managers/MaterialManager.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <string>
+
+namespace Engine::Rendering
+{
+	class Material;
+}
+
+namespace Engine::Managers
+{
+	class MaterialManager
+	{
+	private:
+		MaterialManager();
+
+	public:
+		static bool AddMaterial(std::string id, Engine::Rendering::Material& material);
+
+		static bool MaterialExists(std::string id);
+
+		static Engine::Rendering::Material& GetMaterial(std::string id);
+		
+		static bool RemoveShader(std::string id);
+		static bool RemoveAll();
+	};
+};
